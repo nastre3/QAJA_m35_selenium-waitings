e80b6c666860b72da70144d462a91c1e230d4113
nastre3 <nastre70@gmail.com>
2022-02-28 13:26:32 +1000

    Finished "Expectations"

--- src/test/java/ru/rockstarhamster/selenium/waitings/TestTest.java
+++ src/test/java/ru/rockstarhamster/selenium/waitings/TestTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -12,11 +14,14 @@ import java.time.Duration;
 public class TestTest {
     @Test
     public void open_google() {
-        System.setProperty("webdriver.chrome.driver", "/home/alex/IdeaProjects/selenium-watings/src/test/resources/chromedriver");
+        // использование ожиданий
+        System.setProperty("webdriver.chrome.driver", "C:\\Tools\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // неявное (implicit)
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.elementToBeClickable(By.id("some id")));
-        driver.get("https://google.com");
+        // wait.until(ExpectedConditions.elementToBeClickable(By.id("some id")));
+        driver.get("https://www.aviasales.ru/search/LED1102MOW13021?request_source=search_form&payment_method=all");
+        wait.until(ExpectedConditions.elementToBeClickable(By.className("filter-group"))); // явное (explicit)
+        // driver.get("https://google.com");
     }
 }
